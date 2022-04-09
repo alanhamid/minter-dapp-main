2449c4535c192d9c7017be0827973dd75c209d5d
Alan Hamid <alanhamid13@gmail.com>
2022-04-09 14:21:39 -0400

    Discord link updated

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "Camel Jockey Club"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Camel Jockey Club NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Camel Jockey Club NFT Project is a decentralized, open-source project that aims to make our world better place to live. Come, Join the <a href="https://discord.gg/YFpcGkJq9M"  target="_blank" rel="noopener noreferrer">Camel Jockey Club</a> Discord channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";

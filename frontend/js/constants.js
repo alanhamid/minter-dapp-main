d40384dd07f4ba6a1cd54004d31e06eb9c6a24e9
Alan Hamid <alanhamid13@gmail.com>
2022-04-10 11:48:39 -0400

    updated paragraph

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Camel Jockey Club NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The Camel Jockey Club's main goal is to make our world a better place to live for all. Come, Join the club and build it together <a href="https://discord.gg/YFpcGkJq9M"  target="_blank" rel="noopener noreferrer">Camel Jockey Club</a> Discord channel.';
+const welcome_p = 'The main goal of the Camel Jockey Club is to make our world a better place to live for all. Come, Join the club and build it together <a href="https://discord.gg/YFpcGkJq9M"  target="_blank" rel="noopener noreferrer">Camel Jockey Club</a> Discord channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";

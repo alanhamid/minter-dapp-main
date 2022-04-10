f054123dab8f52b444dc50096c1f8d17bab1d466
Alan Hamid <alanhamid13@gmail.com>
2022-04-10 11:54:21 -0400

    contract address updated

--- backend/src/config.js
+++ backend/src/config.js
@@ -60,7 +60,7 @@ const TOKENS_PER_MINT = 7; // maximum number of NFTs a user can mint in a single
 const PUBLIC_MINT_START_DATE = "2022-04-17T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-16T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xe82e9032fbc5e9cde32fC1C679e4bb896AA30a2f"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
@@ -68,7 +68,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = ["0xe82e9032fbc5e9cde32fC1C679e4bb896AA30a2f"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0xc9b60FbAA9C71003e6441f54A6200efd311898c0"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
